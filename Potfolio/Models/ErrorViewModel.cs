44c11751173f3f883bf7aaec1cf4a609595c42d4
Нургатин Марат Маратович <marat.nurgatin@urfu.me>
2023-10-27 19:11:51 +0500

    Добавьте файлы проекта.

---
+++ Potfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Potfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
